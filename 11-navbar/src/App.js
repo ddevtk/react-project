1905a5557089640d45379a1265bae87474fc0373
duongbui123 <buiducduong2342000@gmail.com>
2021-05-25 20:56:12 +0700

    done 🚀🚀🚀

---
+++ 11-navbar/src/App.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react';
+import Navbar from './Navbar';
+function App() {
+  return (
+    <Fragment>
+      <Navbar />
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ 11-navbar/src/SideBar.js
@@ -0,0 +1,22 @@
+const SideBar = () => {
+  return (
+    <div className='links-container show-container'>
+      <ul className='links'>
+        <li>
+          <a href='#'>Home</a>
+        </li>
+        <li>
+          <a href='#'>About</a>
+        </li>
+        <li>
+          <a href='#'>Contact</a>
+        </li>
+        <li>
+          <a href='#'>Product</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default SideBar;
